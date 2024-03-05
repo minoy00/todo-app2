c9e30b9d122a004b716d735cfa451c46be425beb
minoy00 <min0@gsland153.com>
2024-03-05 20:36:12 +0900

    first commit

---
+++ demo.js
@@ -0,0 +1,35 @@
+// 데이터 타입
+// 문자열 string - "가나다"
+"안녕"
+// 숫자열 number - 1 10
+1;
+// 참거짓 boolean - true false
+true;
+false;
+// 아무것도 없는것 (정의는 되어있지만 없는것) null
+null;
+// 정의되어있지않은 undefined
+undefined;
+
+키:180
+몸무게:70
+피부색:노랗다
+기독교:맞다
+박사학위:없다
+
+키:180;
+몸무게:70;
+피부색:"노란";
+기독교:true;
+박사학위:null;
+
+
+
+// 변수
+height = 180;
+christ = true
+console.log("height=", height, christ); 치면 height=180이랑 true나옴
+// 함수
+// 조건문
+// 반복문
+
